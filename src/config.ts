693b156e7d472db90e8b5e6c888550bc7d44df99
David Legrand <me@davlgd.com>
2025-06-16 14:01:23 +0200

    chore: update to AstroPaper 5.4.2

--- src/config.ts
+++ src/config.ts
@@ -17,6 +17,7 @@ export const SITE = {
     url: "https://github.com/davlgd/labs/tree/main/src/data/blog",
   },
   dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Etc/UTC", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
